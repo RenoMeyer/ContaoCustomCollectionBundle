3537e7a0b80ba94df8e253af8b8aa9506ce9145e
Reno Meyer <reno.meyer@outlook.com>
2017-08-17 10:34:56 +0200

    no template loader

--- Resources/contao/config/autoload.php
+++ Resources/contao/config/autoload.php
@@ -1,7 +1,1 @@
 <?php
-
-TemplateLoader::addFiles([
-    'collection_default' => 'system/modules/custom-collection/templates',
-    'gallery_collection' => 'system/modules/custom-collection/templates',
-    'mod_custom_collection' => 'system/modules/custom-collection/templates',
-]);
